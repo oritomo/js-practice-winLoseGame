3f2fe9601064b1e80a3dbc68621918697d60446a
oritomo <65005751+oritomo@users.noreply.github.com>
2020-05-31 10:41:21 +0900

    Add files via upload
    タイマー機能追加

--- file/main.js
+++ file/main.js
@@ -1,6 +1,6 @@
 'use strict';
 {
-  const num = 5;
+  const num = 10;
   const winner = Math.floor(Math.random() * num);
   const list = document.getElementById('list');
   const start = document.getElementById('start');
@@ -8,13 +8,14 @@
   const score = document.getElementById('score');
   let count = 0;
   question.textContent = 'クリックしたらゲームスタート！'
+  let startTime; //タイマー用変数
 
   start.addEventListener('click', () => {
     question.textContent = '当たりを探してね！'
     start.classList.add('none');
+    startTime = Date.now(); //タイマー起動
     gameStart();
   });
-
 
   function gameStart() {
     for (let i = 0; i < num; i++) {
@@ -26,15 +27,19 @@
         count++;
         score.textContent = `めくった枚数: ${count}`;
         if (winner === i) {
-          div.textContent = 'WIN!'
+          div.textContent = '当たり!'
           div.classList.add('win');
           question.textContent = 'おめでとう！'
+          
+          let timer = ((new Date() - startTime) / 1000).toFixed(0); //タイマー
+          
           setTimeout(() => {
-            window.alert(`当てるのにめっくた枚数は${count}枚です！`)
+            window.alert(`当てるのにめくった枚数は${count}枚です！
+            ${timer}秒かかりました`)
           }, 700);
-
+          return;
         } else {
-          div.textContent = "LOSE!"
+          div.textContent = "はずれ!"
           div.classList.add('lose')
         }
       });
